cbc324e8305d02a6ebb1fd104b9d923ca7b8822c
elena <elena.m.delamano.freije@udc.es>
2015-11-09 19:52:43 +0100

    Test contenido (ANUNCIO)

--- Practica1/src/test/java/es/udc/fic/vvs/Practica1/ContenidoTest.java
+++ Practica1/src/test/java/es/udc/fic/vvs/Practica1/ContenidoTest.java
@@ -1,22 +1,56 @@
 package es.udc.fic.vvs.Practica1;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple Contenido.
  */
-public class ContenidoTest extends TestCase {
+public class ContenidoTest {
     
-	
+	@Test
     public void testCrearAnuncioYObtenerDatos()
     {
         Contenido anuncio = new Anuncio();
         
         assertEquals("PUBLICIDAD", anuncio.obtenerTitulo());
         assertEquals(5, anuncio.obtenerDuracion());
+        
+        List<Contenido> lista = new ArrayList<Contenido>();
+        lista.add(anuncio);
+        
+        assertEquals(1, anuncio.obtenerListaReproduccion().size());
+        assertEquals(lista, anuncio.obtenerListaReproduccion());
+        
     }
     
+	
+	@Test
+	public void testBuscarEnAnuncioCorrecto(){
+		
+		Contenido anuncio = new Anuncio();
+		List<Contenido> lista = new ArrayList<Contenido>();
+		lista.add(anuncio);
+		
+		assertEquals(1, anuncio.buscar("PUBLI").size());
+		assertEquals(lista, anuncio.buscar("PUBLI"));
+		
+	}
+	
+	@Test
+	public void testBuscarEnAnuncioIncorrecto(){
+		
+		Contenido anuncio = new Anuncio();
+		List<Contenido> listaVacia = new ArrayList<Contenido>();
+		
+		
+		assertEquals(0, anuncio.buscar("CANCI").size());
+		assertEquals(listaVacia, anuncio.buscar("CANCI"));
+		
+	}
     
 }
