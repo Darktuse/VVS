544a2dceddb87ca0cfb31adb0ffab64cdb35d924
mateo <mateof@gmail.com>
2015-11-09 19:30:23 +0100

    creado o entorno para os test

--- Practica1/src/test/java/es/udc/fic/vvs/Practica1/AppTest.java
+++ Practica1/src/test/java/es/udc/fic/vvs/Practica1/AppTest.java
@@ -1,37 +1,42 @@
 package es.udc.fic.vvs.Practica1;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+//    extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+	@Test
+    public void appTest()
     {
         assertTrue( true );
     }
