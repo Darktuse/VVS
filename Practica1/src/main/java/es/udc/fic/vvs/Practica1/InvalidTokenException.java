394a18c4711e5fa1a976aa6220a2cc97f2b546ad
mateo <mateof@gmail.com>
2015-11-09 19:45:46 +0100

    Excepcion de token

---
+++ Practica1/src/main/java/es/udc/fic/vvs/Practica1/InvalidTokenException.java
@@ -0,0 +1,9 @@
+package es.udc.fic.vvs.Practica1;
+
+public class InvalidTokenException extends Exception{
+	
+	InvalidTokenException(){
+		System.out.println("Contenido inexistente");
+	}
+
+}
---
+++ Practica1/src/test/java/es/udc/fic/vvs/Practica1/ServidorRespaldoTest.java
@@ -0,0 +1,14 @@
+package es.udc.fic.vvs.Practica1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServidorRespaldoTest {
+
+	private Servidor servidorValid(){
+		Servidor servidorRespaldo = new ServidorImpl();;
+		return servidorRespaldo;
+	}
+	
+}
