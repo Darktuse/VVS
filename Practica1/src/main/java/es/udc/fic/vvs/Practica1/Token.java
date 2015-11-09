ce24e5104d7b4948c2a6d1afa530dd80ce18f828
mateo <mateo.fuentes.pombo@udc.es>
2015-11-09 11:43:31 +0100

    token

---
+++ Practica1/src/main/java/es/udc/fic/vvs/Practica1/Token.java
@@ -0,0 +1,38 @@
+package es.udc.fic.vvs.Practica1;
+
+public class Token {
+
+	String token;
+	int numero;
+	
+	public Token(String token, int numero) {
+		super();
+		this.token = token;
+		this.numero = numero;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public boolean restarUno(){
+		if (numero>0){
+			numero--;
+			return true;
+		}
+		return false;
+	}
+	
+}
