d08933c41a5d1fe5302764631b1fea5bf5c1f478
Darktuse <sueiroposada@gmail.com>
2015-11-09 20:44:23 +0100

    Eliminado AppTest Añadido ServidorTest con pruebas para el servidor sin respaldo

---
+++ Practica1/src/test/java/es/udc/fic/vvs/Practica1/ServidorTest.java
@@ -0,0 +1,104 @@
+package es.udc.fic.vvs.Practica1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Unit test for simple Contenido.
+ */
+public class ServidorTest {
+    
+	private static final String tokenSpecial = "tokenspecial";
+
+	
+	@Test
+    public void obtenerContenidoTest()
+    {
+
+		Servidor servidor = crearServidor();
+		
+		String token = servidor.alta();
+		
+		List<Contenido> contenidos = servidor.buscar("4", token);
+		
+		assertEquals(contenidos.size(), 1);
+		
+    }
+    
+	
+	@Test
+	public void obtenerContenidoSinTokenTest(){
+		
+		Servidor servidor = crearServidor();
+		
+		String token = "No valido";
+	
+		//Debe devolver una lista vacia
+		assert(servidor.buscar("4", token).isEmpty()==true);
+		
+	}
+	
+	@Test
+	public void darBajaTokenTest(){
+		
+		Servidor servidor = crearServidor();
+		
+		String token = servidor.alta();
+		
+		List<Contenido> contenidos = servidor.buscar("4", token);
+		
+		assert(contenidos.isEmpty()==false);
+		
+		servidor.baja(token);
+		
+		contenidos = servidor.buscar("4", token);
+		
+		assert(contenidos.isEmpty()==true);
+	}
+	
+	private Servidor crearServidor(){
+				
+		Servidor servidor = new ServidorImpl("Servidor");
+		Contenido cancion1 = new Cancion("Cancion1", 1);
+		Contenido cancion2 = new Cancion("Cancion2", 2);
+		Contenido cancion3 = new Cancion("Cancion3", 3);
+		Contenido cancion4 = new Cancion("Cancion4", 4);
+		Contenido cancion5 = new Cancion("Cancion5", 3);
+		Contenido cancion6 = new Cancion("Cancion6", 2);
+		Contenido cancion7 = new Cancion("Cancion7", 1);
+		Contenido cancion8 = new Cancion("Cancion8", 4);
+		Contenido emisora1 = new Emisora("Emisora1");
+		Contenido anuncio = new Anuncio();
+		try {
+			emisora1.agregar(cancion7, null);
+			emisora1.agregar(cancion6, cancion7);
+			emisora1.agregar(cancion8, cancion6);
+			emisora1.agregar(cancion1, cancion8);
+			emisora1.agregar(anuncio, cancion6);
+		} catch (ContenidoInexistenteException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			servidor.agregar(cancion1, tokenSpecial);
+			servidor.agregar(cancion2, tokenSpecial);
+			servidor.agregar(cancion3, tokenSpecial);
+			servidor.agregar(cancion4, tokenSpecial);
+			servidor.agregar(cancion5, tokenSpecial);
+			servidor.agregar(cancion6, tokenSpecial);
+			servidor.agregar(emisora1, tokenSpecial);
+		} catch (InvalidTokenException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return servidor;
+	}
+    
+}
